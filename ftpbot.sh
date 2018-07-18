87ac959ebb944cf96baa97b6066614a44a7c9959
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-18 16:44:55 -0400

    Update ftpbot.sh

--- ftpbot.sh
+++ ftpbot.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" 
-while read file; do curl -X POST -H 'Content-Type: application/json' -d '{"username": "test", "content": "hello"}' https://discordapp.com/api/webhooks/467280811843125249/CIbVghvkWUS6AqhVRRG5Ktsm3R2GvFelOfQMjczyc8oHhO0O0SBNMIm0rE2VNOUU_ted
-done 
+inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" | while read file; do curl -X POST -H 'Content-Type: application/json' -d '{"username": "test", "content": "$file"}' https://discordapp.com/api/webhooks/467280811843125249/CIbVghvkWUS6AqhVRRG5Ktsm3R2GvFelOfQMjczyc8oHhO0O0SBNMIm0rE2VNOUU_ted; done
