788e71bc1f68d3a2fff52e6b403671108a4ca251
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-18 16:38:03 -0400

    Update ftpbot.sh

--- ftpbot.sh
+++ ftpbot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-WEBHOOK_URL="https://discordapp.com/api/webhooks/467280811843125249/CIbVghvkWUS6AqhVRRG5Ktsm3R2GvFelOfQMjczyc8oHhO0O0SBNMIm0rE2VNOUU_ted"
-
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" 
 while read file; do 
    PAYLOAD=" {\"content\": \"New upload detected in XCI RARS: $file.\" }"
-   curl -X POST -H 'Content-Type: application/json' -d "$PAYLOAD" "$WEBHOOK_URL"
+   curl -X POST -H 'Content-Type: application/json' -d '{"username": "test", "content": "hello"}' "$WEBHOOK_URL"
 done 
