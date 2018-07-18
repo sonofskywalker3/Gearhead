f0606e2f31f442472a339bf79b1179d03cc8aa45
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-18 17:35:51 -0400

    Update ftpbot.sh

--- ftpbot.sh
+++ ftpbot.sh
@@ -4,6 +4,6 @@ URL="https://discordapp.com/api/webhooks/467280811843125249/CIbVghvkWUS6AqhVRRG5
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" | while read file; 
 do
-  PAYLOAD='{"content":"$file"}'
+  PAYLOAD="'{\"content\":\"$file\"}'"
   curl -X POST -H 'Content-Type: application/json' -d $PAYLOAD $URL; 
 done
