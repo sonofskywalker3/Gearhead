210d83719b6c13a8bacaddd95d016d6168f51617
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 16:32:57 -0400

    Update updatebot.sh

--- updatebot.sh
+++ updatebot.sh
@@ -4,6 +4,6 @@ URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXw
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/UPDATES | while read FILE; 
 do
-  PAYLOAD="{\"content\":\"New Update NSP upload detected: $FILE\"}"
+  PAYLOAD="{\"content\":\"***New Update NSP upload detected:*** $FILE\"}"
    curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$URL"; 
 done
