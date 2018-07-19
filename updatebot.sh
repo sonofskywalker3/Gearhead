ada8a1901041dbfae4b9dc70c473094002e50c4b
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 10:20:06 -0400

    Update updatebot.sh

--- updatebot.sh
+++ updatebot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="https://discordapp.com/api/webhooks/469260299774459906/-aIVW5t5UwV6fc_oBQYdNKDyrOvA48cJ4pgzPBWnsNlycfdbeiVuemVm-c_KkK1gtofn"
+URL="https://discordapp.com/api/webhooks/469508611026976768/7yqzlk3Dt4089BVL1oYIDySD9pp2O_BVMPnAL9iC4X6PyyrSK2IpTgV65LUWFdmXQIGw"
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/UPDATES | while read FILE; 
 do
