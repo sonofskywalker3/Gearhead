adb94ed3a45ee4f7569c83843e518cf320aaa673
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 10:45:45 -0400

    Update nspbot.sh

--- nspbot.sh
+++ nspbot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-NSPURL="https://discordapp.com/api/webhooks/469260536991711250/HU3FVRnPW-UZTYHf8hywCTmvobhHFHjAr0_1JN0_5hIceCX9A4y5rmXpYFk1qv0QcsZQ"
+URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXwWGBK0tkRO1Y059XMmCJreQH9AtJMFoXfEsP0lG51p27fz2-O"
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/NSP | while read file; 
 do
-  PAYLOAD="{\"content\":\"$file\"}"
-   curl -H "Content-Type: application/json" -X POST -d $PAYLOAD $NSPURL; 
+  PAYLOAD="{\"content\":\"New NSP upload detected: $file\"}"
+   curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" $NSPURL; 
 done
