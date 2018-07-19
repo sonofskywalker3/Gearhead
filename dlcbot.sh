726fbbd3376cd6be121206360033ecece301373f
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 10:24:18 -0400

    Create dlcbot.sh

---
+++ dlcbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXwWGBK0tkRO1Y059XMmCJreQH9AtJMFoXfEsP0lG51p27fz2-O"
+
+inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/DLC | while read FILE; 
+do
+  PAYLOAD="{\"content\":\"$FILE\"}"
+   curl -H "Content-Type: application/json" -X POST -d $PAYLOAD $URL; 
+done
