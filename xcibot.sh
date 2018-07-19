eabdc9f28eff5472302adaa769e494f57abfbb74
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 10:13:44 -0400

    Update and rename ftpbot.sh to xcibot.sh

--- xcibot.sh
+++ xcibot.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 URL="https://discordapp.com/api/webhooks/469260299774459906/-aIVW5t5UwV6fc_oBQYdNKDyrOvA48cJ4pgzPBWnsNlycfdbeiVuemVm-c_KkK1gtofn"
-NSPURL="https://discordapp.com/api/webhooks/469260299774459906/-aIVW5t5UwV6fc_oBQYdNKDyrOvA48cJ4pgzPBWnsNlycfdbeiVuemVm-c_KkK1gtofn"
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" | while read file; 
 do
   PAYLOAD="{\"content\":\"$file\"}"
    curl -H "Content-Type: application/json" -X POST -d $PAYLOAD $URL; 
-done
-
-inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/NSP | while read file; 
-do
-  PAYLOAD="{\"content\":\"$file\"}"
-   curl -H "Content-Type: application/json" -X POST -d $PAYLOAD $NSPURL; 
 done
