50e1fd87785378a357aefd75e33a3668b6961293
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-19 10:13:30 -0400

    Create NSPbot.sh

---
+++ NSPbot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NSPURL="https://discordapp.com/api/webhooks/469260299774459906/-aIVW5t5UwV6fc_oBQYdNKDyrOvA48cJ4pgzPBWnsNlycfdbeiVuemVm-c_KkK1gtofn"
+
+inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/NSP | while read file; 
+do
+  PAYLOAD="{\"content\":\"$file\"}"
+   curl -H "Content-Type: application/json" -X POST -d $PAYLOAD $NSPURL; 
+done
