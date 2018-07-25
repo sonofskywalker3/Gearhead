b8594892f902a1b6bd4475d3c14341700f00130a
Jeff Moore <sonofskywalker3@gmail.com>
2018-07-25 12:01:39 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-nohup ./xcibot.sh 0<&- &>~/scripts/Gearhead/xcibot.txt &
-nohup ./nspbot.sh 0<&- &>~/scripts/Gearhead/nspbot.txt &
-nohup ./dlcbot.sh 0<&- &>~/scripts/Gearhead/dlcbot.txt &
-nohup ./updatebot.sh 0<&- &>~/scripts/Gearhead/updatebot.txt &
+nohup ./xcibot.sh 0<&- &>/dev/null &
+nohup ./nspbot.sh 0<&- &>/dev/null &
+nohup ./dlcbot.sh 0<&- &>/dev/null &
+nohup ./updatebot.sh 0<&- &>/dev/null &
+nohup ./sxosbot.sh 0<&- &>/dev/null &
+nohup ~/python/python3/bin/python3 ~/scripts/Gearhead/bot.py &
