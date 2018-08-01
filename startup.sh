9eeab4acb9c30d2ccc1bdd73cae369d0c18bf344
Jeff Moore <sonofskywalker3@gmail.com>
2018-08-01 14:05:39 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,4 +5,10 @@ nohup ./nspbot.sh 0<&- &>/dev/null &
 nohup ./dlcbot.sh 0<&- &>/dev/null &
 nohup ./updatebot.sh 0<&- &>/dev/null &
 nohup ./sxosbot.sh 0<&- &>/dev/null &
+nohup ./demobot.sh 0<&- &>/dev/null &
+nohup ./demoupdbot.sh 0<&- &>/dev/null &
+nohup ./nsxbot.sh 0<&- &>/dev/null &
+nohup ./switchosbot.sh 0<&- &>/dev/null &
+nohup ./managerbot.sh 0<&- &>/dev/null &
+nohup ./nsxbot.sh 0<&- &>/dev/null &
 nohup ~/python/python3/bin/python3 ~/scripts/Gearhead/bot.py &
