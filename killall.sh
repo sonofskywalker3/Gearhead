bb8302f7f8a6ce380773aad45f80a15207cbbb27
Jeff Moore <sonofskywalker3@gmail.com>
2018-08-01 10:53:08 -0400

    Update killall.sh

--- killall.sh
+++ killall.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+killall -9 xcibot.sh -u $(whoami)
+killall -9 nspbot.sh -u $(whoami)
+killall -9 updatebot.sh -u $(whoami)
+killall -9 dlcbot.sh -u $(whoami)
+killall -9 sxosbot.sh -u $(whoami)
+killall -9 demobot.sh -u $(whoami)
+killall -9 demoupdbot.sh -u $(whoami)
+killall -9 nsxbot.sh -u $(whoami)
+killall -9 switchosbot.sh -u $(whoami)
+killall -9 managerbot.sh -u $(whoami)
