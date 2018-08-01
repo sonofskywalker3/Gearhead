4d2c2c4365b031c06b4052406d1040a16fb7e510
Jeff Moore <sonofskywalker3@gmail.com>
2018-08-01 12:07:45 -0400

    Update nsxbot.sh

--- nsxbot.sh
+++ nsxbot.sh
@@ -4,6 +4,6 @@ URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXw
 
 inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0000 - (NX-eShop)"/"0002 - (NSX)" | while read FILE; 
 do
-  PAYLOAD="{\"content\":\"***New NSP Demo upload detected:*** \`\`\`$FILE\`\`\`\"}"
+  PAYLOAD="{\"content\":\"***New NSX upload detected:*** \`\`\`$FILE\`\`\`\"}"
    curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$URL"; 
 done
