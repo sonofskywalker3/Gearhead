6fd1b478990421c11a1034b142f17c55f9cc6045
Jeff Moore <sonofskywalker3@gmail.com>
2018-08-01 10:56:49 -0400

    Update dlcbot.sh

--- dlcbot.sh
+++ dlcbot.sh
@@ -2,7 +2,7 @@
 
 URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXwWGBK0tkRO1Y059XMmCJreQH9AtJMFoXfEsP0lG51p27fz2-O"
 
-inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW eShop)"/DLC | while read FILE; 
+inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0000 - (NX-eShop)"/"0001 - (DLC)" | while read FILE; 
 do
   PAYLOAD="{\"content\":\"***New NSP DLC upload detected:*** \`\`\`$FILE\`\`\`\"}"
    curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$URL"; 
