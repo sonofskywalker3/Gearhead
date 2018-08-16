553cfe243f767e4e65dd0cb81f796f0df26b18f8
Jeff Moore <sonofskywalker3@gmail.com>
2018-08-16 16:32:29 -0400

    Update nspbot.sh

--- nspbot.sh
+++ nspbot.sh
@@ -2,7 +2,7 @@
 
 URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXwWGBK0tkRO1Y059XMmCJreQH9AtJMFoXfEsP0lG51p27fz2-O"
 
-inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0000 - (NX-eShop)"/"0000 - (NSP Games)" | while read file; 
+inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0000 - (NX-eShop)"/"0003 - (New NSP)" | while read file; 
 do
   PAYLOAD="{\"content\":\"***New NSP Game upload detected:*** \`\`\`$file\`\`\`\"}"
    curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$URL"; 
