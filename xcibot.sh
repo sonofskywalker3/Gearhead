88dbd240449e6a410f6a9ae474a68f367f405c50
Jeff Moore <sonofskywalker3@gmail.com>
2018-11-26 11:57:58 -0500

    Update xcibot.sh

--- xcibot.sh
+++ xcibot.sh
@@ -2,7 +2,7 @@
 
 URL="https://discordapp.com/api/webhooks/469509650954452992/-kaxqQKrrtSP0UzKzGXwWGBK0tkRO1Y059XMmCJreQH9AtJMFoXfEsP0lG51p27fz2-O"
 
-inotifywait --monitor --format %f -e moved_to -e create ~/NSW/"0001 - (NSW XCI RAR)" | while read file; 
+inotifywait --monitor --format %f -e moved_to -e create /mnt/md4/XCI | while read file; 
 do
   PAYLOAD="{\"content\":\"***New XCI upload detected:*** \`\`\`$file\`\`\`\"}"
    curl -H "Content-Type: application/json" -X POST -d "$PAYLOAD" "$URL"; 
